cd030148bc231b10b723e45586df838d8428b9f4
zulham724 <zulham724@students.unnes.ac.id>
2018-07-31 10:41:46 +0700

    change seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,18 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        role::create([
-        	"id"=>1,
-        	"content"=>"admin"
-        	]);
-        role::create([
-        	"id"=>2,
-        	"content"=>"operator"
-        	]);
-          role::create([
-            "id"=>3,
-            "content"=>"staff"
-          ]);
+        // role::create([
+        // 	"id"=>1,
+        // 	"content"=>"admin"
+        // 	]);
+        // role::create([
+        // 	"id"=>2,
+        // 	"content"=>"operator"
+        // 	]);
+        //   role::create([
+        //     "id"=>3,
+        //     "content"=>"staff"
+        //   ]);
           User::create([
               'name' => "Admin",
               'email' => "admin@gmail.com",
