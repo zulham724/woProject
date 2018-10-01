522eae2f5ab96620aafe20e88f381bc8feb1cba5
Diki <dikydharmawan62@gmail.com>
2018-10-01 16:15:20 +0700

    fixing

--- resources/views/kursus/index.blade.php
+++ resources/views/kursus/index.blade.php
@@ -83,20 +83,22 @@
 						<img src="{{asset('img/logo/logo.png')}}" />
 						<h4>Wedding & Event Organizer</h4>
 						<h5>Jl.Pandanaran 126.Ruko Masjid Baiturrahman,Simpang Lima ,Semarang. Telp (024) 8313313</h5>
-					</div><hr>
+					</div><hr style="height: 3px;border:none;color: #333;background-color: #333">
 					<div class="text-center">
-						<h1 style="font-family: Bookman Old Style"><b><u>SERTIFIKAT</u></b></h1><br>
+						
+						<p style="font-size:50px; font-family: Bookman Old Style"><b><u>SERTIFIKAT</u></b></p><br>
 						<h4>Diberikan kepada :</h4>
 						@php
 						setlocale (LC_TIME, 'id');
 						$date = strftime( "%d %B %Y", strtotime($course->date));
+						$date1 = strftime( " %d %B %Y", time());
 						$kalimat = $course->name;
 						$kalimat_new = ucwords($kalimat);
 						$tipe = $course->courses_list->type;
 						$tipe_new = ucwords($tipe)
 						@endphp
 						<font face="Comic Sans MS">
-							<h2><u>{{$kalimat_new}}</u></h2><br>
+							<h1><u>{{$kalimat_new}}</u></h1><br>
 						</font>
 						<h4>Telah menyelesaikan program kursus {{$tipe_new}}<br>
 							yang dilaksanakan pada tanggal {{$date}}<br>
@@ -107,21 +109,23 @@
 					
 	      	     	
 					
-					<div class="row" style="height:100px">
+					<div class="row" style="height:150px">
 						<div class="col-xs-12">
-							<div class="col-xs-9">
+							<div class="col-xs-8">
 							</div>
-							<div class="col-xs-3">
-								<h4><center>Ketua LPK</center></h4>
+							<div class="col-xs-4">
+								<h4>
+									<center>Semarang, {{$date1}} <br>
+									Ketua LPK</center></h4>
 							</div>
 							
 						</div>
 					</div>
 					<div class="row" style="height:10px">
 						<div class="col-xs-12">
-							<div class="col-xs-9">
+							<div class="col-xs-8">
 							</div>
-							<div class="col-xs-3">
+							<div class="col-xs-4">
 								<h4><center>Success</center> </h4>
 							</div>
 							
